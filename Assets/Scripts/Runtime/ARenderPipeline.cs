627e83a1f42229fd91724cd81fa56be5207c6a5e
ccandy <ccunandy2001@gmail.com>
2025-04-07 13:58:24 +0800

    Add CameraRender

--- Assets/Scripts/Runtime/ARenderPipeline.cs
+++ Assets/Scripts/Runtime/ARenderPipeline.cs
@@ -5,8 +5,12 @@ using UnityEngine.Rendering;
 
 public class ARenderPipeline : RenderPipeline
 {
+    private CameraRender _cameraRender = new CameraRender();
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
-        
+        for (int n = 0; n < cameras.Length; n++)
+        {
+            _cameraRender.Render(context, cameras[n]);
+        }
     }
 }
---
+++ Assets/Scripts/Runtime/CameraRender.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRender
+{
+   ScriptableRenderContext context;
+   Camera camera;
+   const string bufferName = "CameraRender";
+   private CommandBuffer cmd;
+   private CullingResults _cullingResults;
+   private static ShaderTagId shaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+   private static ShaderTagId[] unsupportShaderTagId =
+   {
+      new ShaderTagId("Always"),
+      new ShaderTagId("ForwardBase"),
+      new ShaderTagId("PrepassBase"),
+      new ShaderTagId("Vertex"),
+      new ShaderTagId("VertexLM"),
+      new ShaderTagId("VertexLMU"),
+      new ShaderTagId("VertexLMRGBM"),
+   };
+
+   private Material _errMaterial;
+   
+   public void Render(ScriptableRenderContext context, Camera camera)
+   {
+      this.context = context;
+      this.camera = camera;
+
+      cmd = new CommandBuffer()
+      {
+         name = bufferName
+      };
+
+      if (!Cull())
+      {
+         return;
+      }
+      Setup();
+      DrawVisibleGeometry(this.camera);
+      DrawUnsupportedGeometry(this.camera);
+      Submit();
+   }
+
+   bool Cull()
+   {
+      ScriptableCullingParameters p;
+
+      if (camera.TryGetCullingParameters(out p))
+      {
+         _cullingResults = context.Cull(ref p);
+         return true;
+      }
+      return false;
+   }
+
+   void Setup()
+   {
+      context.SetupCameraProperties(camera);
+      cmd.ClearRenderTarget(true, true, Color.clear);
+      BeginSample();
+   }
+   
+
+   void Submit()
+   {
+      EndSample();
+      context.Submit();
+   }
+
+   void DrawVisibleGeometry(Camera camera)
+   {
+      DrawOpaqueGeometry(camera);
+      context.DrawSkybox(camera);
+      DrawTransparentGeometry(camera);
+   }
+
+
+   void DrawOpaqueGeometry(Camera camera)
+   {
+      var sortingSettings = new SortingSettings(camera)
+      {
+         criteria = SortingCriteria.CommonOpaque
+      };
+      var drawingSettings = new DrawingSettings(shaderTagId, sortingSettings);
+      var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+      context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+   }
+
+   void DrawTransparentGeometry(Camera camera)
+   {
+      var sortingSettings = new SortingSettings(camera)
+      {
+         criteria = SortingCriteria.CommonTransparent
+      };
+      
+      var drawingSettings = new DrawingSettings(shaderTagId, sortingSettings);
+      var filteringSettings = new FilteringSettings(RenderQueueRange.transparent);
+      context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+   }
+
+   void DrawUnsupportedGeometry(Camera camera)
+   {
+
+      if (_errMaterial == null)
+      {
+         _errMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+      }
+      
+      var sortingSettings = new SortingSettings(camera);
+      var filteringSettings = FilteringSettings.defaultValue;
+      var drawingSettings = new DrawingSettings(unsupportShaderTagId[0], sortingSettings)
+      {
+         overrideMaterial = _errMaterial
+      };
+
+      for (int i = 0; i < unsupportShaderTagId.Length; i++)
+      {
+         var shaderTagId = unsupportShaderTagId[i];
+         drawingSettings.SetShaderPassName(i,shaderTagId);
+      }
+      
+      context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+   }
+   
+   void BeginSample()
+   {
+      cmd.BeginSample(bufferName);
+      ExecuteCommandBuffer(cmd);
+   }
+
+   void EndSample()
+   {
+      cmd.EndSample(bufferName);
+      ExecuteCommandBuffer(cmd);
+   }
+
+   void ExecuteCommandBuffer(CommandBuffer commandBuffer)
+   {
+      context.ExecuteCommandBuffer(cmd);
+      cmd.Clear();
+   }
+   
+}
