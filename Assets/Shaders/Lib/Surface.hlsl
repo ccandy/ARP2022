ace164660c5373359300234e40b5a231000551df
ccandy <ccunandy2001@gmail.com>
2025-04-08 17:58:44 +0800

    Update Surface add GetSurface hlsl

--- Assets/Shaders/Lib/Surface.hlsl
+++ Assets/Shaders/Lib/Surface.hlsl
@@ -5,3 +5,14 @@ struct Surface
     float4 baseColor;
     float3 normal;
 };
+
+
+Surface GetSurface(float baseColor, float3 normal)
+{
+    Surface s;
+
+    s.baseColor = baseColor;
+    s.normal = normal;
+
+    return s;
+}
