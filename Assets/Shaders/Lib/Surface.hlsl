2eded15a0e9bfba55b0d7ebffa899b9893888ec6
ccandy <ccunandy2001@gmail.com>
2025-04-11 11:04:43 +0800

    update surface add alpha

--- Assets/Shaders/Lib/Surface.hlsl
+++ Assets/Shaders/Lib/Surface.hlsl
@@ -2,17 +2,19 @@
 
 struct Surface
 {
-    float4 baseColor;
+    float3 baseColor;
     float3 normal;
+    float alpha;
 };
 
 
-Surface GetSurface(float baseColor, float3 normal)
+Surface GetSurface(float4 baseColor, float3 normal)
 {
     Surface s;
 
     s.baseColor = baseColor;
     s.normal = normal;
+    s.alpha = baseColor.a;
 
     return s;
 }
