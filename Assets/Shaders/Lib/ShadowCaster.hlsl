d37cd3cf5b32257f5ea27c6edfd9150e5ec1792a
ccandy <ccunandy2001@gmail.com>
2025-06-05 19:28:16 +0800

    Add shadow caster

---
+++ Assets/Shaders/Lib/ShadowCaster.hlsl
@@ -0,0 +1,43 @@
+#pragma once
+
+float4 _Color;
+
+TEXTURE2D(_MainTex);
+SAMPLER(sampler_MainTex);
+
+float4 _MainTex_ST;
+float _CutOff;
+
+struct VertexInput
+{
+    float4 PositionOS : POSITION;
+    float2 uv : TEXCOORD0;
+};
+
+struct VertexOutput
+{
+    float4 PositionCS : SV_POSITION;
+    float2 uv : VAR_BASE_UV;
+};
+
+VertexOutput ShadowCasterVertex( VertexInput input )
+{
+    VertexOutput output;
+
+    float3 worldPos = TransformObjectToWorld(input.PositionOS.xyz);
+    output.PositionCS = TransformWorldToHClip(worldPos);
+    output.uv = TRANSFORM_TEX(input.uv,_MainTex);
+    return output;
+}
+
+
+float4 ShadowCasterFrag( VertexOutput input ) :SV_TARGET
+{
+    float4 baseMap = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, input.uv);
+    float4 color = _Color;
+    float4 baseColor = baseMap * color;
+    #if defined(_CLIPPING)
+        clip(baseColor.a - _CutOff);
+    #endif
+    return baseColor;
+}
