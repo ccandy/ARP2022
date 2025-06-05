aafb0a701ebc498ac2ef505f79ede643bd28a5c7
ccandy <ccunandy2001@gmail.com>
2025-06-05 19:50:22 +0800

    Add function setup shadowdata

--- Assets/Scripts/Runtime/Shadow/ShadowRender.cs
+++ Assets/Scripts/Runtime/Shadow/ShadowRender.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ShadowRender 
 {
-    private int ShadowToWorldCascadeMatID   = Shader.PropertyToID("_ShadowToWorldCascadeMat");
-    private const string bufferName         = "ShadowBuffer";
-    private const int MAX_DIRECTIONS_SHADOW_LIGHTS = 4;
+    private int ShadowToWorldCascadeMatID                   = Shader.PropertyToID("_ShadowToWorldCascadeMat");
+    private const string bufferName                         = "ShadowBuffer";
+    private const int MAX_DIRECTIONS_SHADOW_LIGHTS          = 4;
+
+    public ShadowGlobalData GlobalShadowData                = new ShadowGlobalData();
+    private DirectionalShadowData[] _directionalShadowData  = new DirectionalShadowData[MAX_DIRECTIONS_SHADOW_LIGHTS];
+    
+    private Matrix4x4[] ShadowToWorldCascadeMats            = new Matrix4x4[MAX_DIRECTIONS_SHADOW_LIGHTS];
+    
+
+
+    public void SetupShadowData(float shadowDistance, ref VisibleLight visibleLight, int index)
+    {
+        GlobalShadowData.ShadowDistance = shadowDistance;
+        if (index >= MAX_DIRECTIONS_SHADOW_LIGHTS)
+        {
+            return;
+        }
+
+        _directionalShadowData[index] = new DirectionalShadowData()
+        {
+            ShadowStrength  = visibleLight.light.shadowStrength,
+            ShadowBias      = visibleLight.light.shadowBias,
+            ShadowNearPlane = visibleLight.light.shadowNearPlane
+        };
+    }
+    
     
     
     
