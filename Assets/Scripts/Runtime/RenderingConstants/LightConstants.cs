7790a885e117c84ea709ea6308d8ad8352e4d9d8
ccandy <ccunandy2001@gmail.com>
2025-06-14 11:50:14 +0800

    Add RenderConstants

---
+++ Assets/Scripts/Runtime/RenderingConstants/LightConstants.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LightConstants
+{
+    public static readonly int MAX_DIRECTIONAL_LIGHTS = 4;
+}
