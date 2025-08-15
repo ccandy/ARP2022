4b306a4a94cbb3bc623bd5ff48dbc5e6b1ef2b68
ccandy <ccunandy2001@gmail.com>
2025-08-14 17:00:51 -0700

    Add RenderUtil

---
+++ Assets/Scripts/Runtime/Util/RenderUtil.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RenderUtil
+{
+    public static void SetupRenderTarget(ref ScriptableRenderContext context, int renderTargetId, CommandBuffer cmd)
+    {
+        cmd.SetRenderTarget(renderTargetId, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store);
+        cmd.ClearRenderTarget(true,false, Color.clear);
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+    }
+
+    public void GetRenderTexture(ref ScriptableRenderContext context, int renderTextureID, int width, int height, 
+        int depth, CommandBuffer cmd, 
+        FilterMode filterMode = FilterMode.Bilinear, RenderTextureFormat format = RenderTextureFormat.ARGB32,
+        bool clearColor = false, bool clearDepth = true)
+    {
+        if (cmd == null)
+        {
+            Debug.LogWarning("GetRenderTexture: cmd is null");
+            return;
+        }
+        
+        cmd.GetTemporaryRT(renderTextureID, width, height, depth, filterMode, format);
+        cmd.ClearRenderTarget(clearDepth, clearColor, Color.clear);
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+    }
+}
