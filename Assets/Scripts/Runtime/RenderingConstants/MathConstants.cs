04df3e0f23d9b84adabe31794859de0eb68074ea
ccandy <ccunandy2001@gmail.com>
2025-09-14 10:27:40 -0700

    Add MathConstants

---
+++ Assets/Scripts/Runtime/RenderingConstants/MathConstants.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARP.Constant
+{
+    public static class MathConstant
+    {
+        public const float PI       = 3.14159265358979323846f;
+        public const float SQRT2    = 1.4142135623731f;
+    }
+}
+    
