c535467cae3338c2bf3e56241121788b97a1f5a8
ccandy <ccunandy2001@gmail.com>
2025-09-14 11:22:57 -0700

    Add ShadowKeywords

---
+++ Assets/Scripts/Runtime/RenderingConstants/ShadowKeywords.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARP.Constant
+{
+    public static class ShadowKeywords
+    {
+        public readonly static string[] DirectionalSoftShadowKeyword =
+        {
+            "ENABLE_DIRECTIONAL_SOFTSHADOW_PCF2X2",
+            "ENABLE_DIRECTIONAL_SOFTSHADOW_PCF3X3",
+            "ENABLE_DIRECTIONAL_SOFTSHADOW_PCF5X5",
+            "ENABLE_DIRECTIONAL_SOFTSHADOW_PCF7X7",
+        };
+    }
+}
+
