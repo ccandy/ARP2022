a4ee9dd6fdeffc3cff2474de70ea5933d1bfa04e
ccandy <ccunandy2001@gmail.com>
2025-09-16 13:30:16 -0700

    add light render interface

---
+++ Assets/Scripts/Runtime/Light/Interface/LightRenderInterface.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public interface LightRenderInterface
+{
+   public void ConfigDirectionalLightData(VisibleLight visibleLight);
+   public void CleanUp();
+   public void SendToGPU(ref ScriptableRenderContext context, CommandBuffer cmd);
+
+   public void Render(ref ScriptableRenderContext context, ref CullingResults cullingResults,
+      ref ShadowGlobalData shadowGlobalData);
+
+}
