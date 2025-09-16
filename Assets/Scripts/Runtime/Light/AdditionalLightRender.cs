61c6332a3a1180e170378b6c714c4b5e8b9a196c
ccandy <ccunandy2001@gmail.com>
2025-09-16 14:28:49 -0700

    add AdditionalLightRender

---
+++ Assets/Scripts/Runtime/Light/AdditionalLightRender.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AdditionalLightRender : LightRenderInterface
+{
+    // Start is called before the first frame update
+
+    public void ConfigDirectionalLightData(VisibleLight visibleLight)
+    {
+        
+    }
+
+    public void CleanUp()
+    {
+        
+    }
+
+    public void SendToGPU(ref ScriptableRenderContext context, CommandBuffer cmd)
+    {
+        
+    }
+
+    public void Render(ref ScriptableRenderContext context, ref CullingResults cullingResults,
+        ref ShadowGlobalData shadowGlobalData)
+    {
+        
+    }
+}
