044a5f647dd7f1d94592e55456c37d61960e6a35
ccandy <ccunandy2001@gmail.com>
2025-10-12 16:55:48 -0700

    Fix a bug in DirectionalLightRender.cs

--- Assets/Scripts/Runtime/Light/DirectionalLightRender.cs
+++ Assets/Scripts/Runtime/Light/DirectionalLightRender.cs
@@ -35,36 +35,35 @@ namespace ARP.Render
         public void CleanUp()
         {
             System.Array.Clear(_directionalLightDatas, 0, _directionalLightDatas.Length);
+            directionalLightCount = 0;
         }
 
         public void SendToGPU(ref ScriptableRenderContext context, CommandBuffer cmd)
         {
-
-            if (directionalLightCount <= 0)
+            if (directionalLightCount != 0)
             {
-                return;
-            }
+                Vector4[] dirLightColor = new Vector4[directionalLightCount];
+                Vector4[] dirLightDir   = new Vector4[directionalLightCount];
+                Vector4[] dirLightData  = new Vector4[directionalLightCount];
             
-            Vector4[] dirLightColor = new Vector4[directionalLightCount];
-            Vector4[] dirLightDir   = new Vector4[directionalLightCount];
-            Vector4[] dirLightData  = new Vector4[directionalLightCount];
-            
-            for (int i = 0; i < directionalLightCount; ++i)
-            {
-                dirLightColor[i] = _directionalLightDatas[i].LightColor;
-                dirLightDir[i]      = _directionalLightDatas[i].LightAxis;
+                for (int i = 0; i < directionalLightCount; ++i)
+                {
+                    dirLightColor[i] = _directionalLightDatas[i].LightColor;
+                    dirLightDir[i]      = _directionalLightDatas[i].LightAxis;
 
-                Vector4 lightData   = new Vector4();
-                lightData.x         = _directionalLightDatas[i].LightAtten;
-                lightData.y         = _directionalLightDatas[i].RenderLayerMask;
+                    Vector4 lightData   = new Vector4();
+                    lightData.x         = _directionalLightDatas[i].LightAtten;
+                    lightData.y         = _directionalLightDatas[i].RenderLayerMask;
                 
-                dirLightData[i]     = lightData;
+                    dirLightData[i]     = lightData;
+                }
+            
+                cmd.SetGlobalVectorArray(LightConstants.DirectionalLightsColorId,dirLightColor);
+                cmd.SetGlobalVectorArray(LightConstants.DirectionalLightsDirId, dirLightDir);
+                cmd.SetGlobalVectorArray(LightConstants.DirectionalLightsDataId, dirLightData);
             }
             
-            cmd.SetGlobalVectorArray(LightConstants.DirectionalLightsColorId,dirLightColor);
-            cmd.SetGlobalVectorArray(LightConstants.DirectionalLightsDirId, dirLightDir);
             cmd.SetGlobalInt(LightConstants.DirectonalLightAccountId, directionalLightCount);
-            cmd.SetGlobalVectorArray(LightConstants.DirectionalLightsDataId, dirLightData);
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
         }
